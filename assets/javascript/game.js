e50a72f93769a9b666ccd3e81ec462b01bcf9c70
Blazer <sstubbs012@gmail.com>
2018-08-23 21:34:46 -0400

    changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,13 +6,15 @@ var randNum = Math.floor(Math.random() * diffSports.length);
 var rightWord = [];
 var wrongWord = [];
 var underNumber = [];
+var winCount = 0;
+var guessesLeft = 9;
 var docUnderScore = document.getElementsByClassName("underscore");
-var docRightGuess = document.getElementsByClassName("wins");
+var docRightGuess = document.getElementsByClassName("correct");
 var docWrongGuess = document.getElementsByClassName("guessed");
+
 
 var currentWord = diffSports[randNum];
 console.log(currentWord);
-
 
 
 //create underscores based on length of word
@@ -25,24 +27,54 @@ var underAmount = function() {
 
 console.log(underAmount());
 
+var winner = function() {
+    guessesLeft = 9;
+    wrongWord = [];
+    rightWord = [];
+};
+
+var loser = function() {
+    alert("Game over -- You Lose");
+    guessesLeft = 9;
+    wrongWord = [];
+    rightWord = [];
+    winCount = 0;
+}
+
 //user guess
 document.onkeyup = function(event) {
     var userGuess = event.key;
-// if user guess is right
-    if (currentWord.indexOf(userGuess) > -1) {
-        rightWord.push(userGuess);    
-        underNumber[currentWord.indexOf(userGuess)] = userGuess;
-        docUnderScore[0].innerHTML = underNumber.join(' ');
-        docRightGuess[0].innerHTML = rightWord;
-        //see if user word matches guesses
-        if(underNumber.join('') == currentWord) {
-            alert('You Win!')
-        }
-        console.log(rightWord);
-}
-    else {
-        wrongWord.push(userGuess);
-        docWrongGuess[0].innerHTML = wrongWord;
-}};
 
-//underNumber[0].innerHTML = underAmount.join(' ');
+        // if user guess is right
+        if (currentWord.indexOf(userGuess) > -1) {
+            rightWord.push(userGuess);
+
+            underNumber[currentWord.indexOf(userGuess)] = userGuess;
+
+            docUnderScore[0].innerHTML = underNumber.join(' ');
+            
+            docRightGuess[0].innerHTML = rightWord;
+
+            //see if user word matches guesses
+            if(underNumber.join('') == currentWord) {
+                alert('You Win!');
+                winCount++;
+                var userWins = document.getElementById("count");
+                userWins.innerHTML = winCount;
+                winner();
+            }
+            console.log(rightWord);
+        }
+        else {
+            wrongWord.push(userGuess);
+            docWrongGuess[0].innerHTML = wrongWord;
+            guessesLeft--;
+            var userRemainingGuesses = document.getElementById("remaining");
+            userRemainingGuesses.innerHTML = guessesLeft;
+        }
+        if (guessesLeft === 0) {
+            loser();
+        }
+};
+
+
